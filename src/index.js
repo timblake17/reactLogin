3d5d1bdfe3f12fc92e2c9a4afdf7568defc584e4
Tim Blake <timblake17x@gmail.com>
2017-12-01 16:02:02 -0500

    redux added email and password console logging

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import "semantic-ui-css/semantic.min.css";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from "./rootReducer";
 
+const store = createStore(
+  rootReducer,applyMiddleware(thunk));
 ReactDOM.render(
 <BrowserRouter>
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 </BrowserRouter>,
 document.getElementById('root'));
 registerServiceWorker();
